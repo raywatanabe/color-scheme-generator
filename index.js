506d4a7638a1a18c22fe397e24517cf55dcef612
raywatanabe <raywatanabe23@gmail.com>
2023-09-28 15:03:58 -0600

    Add comments

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const getBtn = document.getElementById("get-btn")
 getBtn.addEventListener("click", () => renderScheme())
 
 function renderScheme(){
+    // Removes # from hex value for url compatibility
     const seedColor = colorInput.value.slice(1)
     const mode = selectMode.value
 
+    // Generates color scheme based on chosen parameters
     fetch(`https://www.thecolorapi.com/scheme?hex=${seedColor}&mode=${mode}`)
         .then(res => res.json())
         .then(data => {
@@ -17,13 +19,16 @@ function renderScheme(){
 
             document.getElementById("main").innerHTML = ""
             hexArray.forEach(hex => {
+                // Creates div displaying color as background
                 const color = document.createElement("div")
                 color.style.backgroundColor = hex
 
+                // Adds text of hex value for users
                 const hexValue = document.createElement("p")
                 hexValue.innerText = hex
                 color.append(hexValue)
 
+                // Copies hex value to clipboard on click
                 color.addEventListener("click", function() {
                     navigator.clipboard.writeText(hex)
                     alert("Copied to clipboard")
